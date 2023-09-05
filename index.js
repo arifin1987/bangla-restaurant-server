b7eb60f760dc0c9024584bb6ea15ca2117fb9f62
Shohidul Arifin <shohidularifin@gmail.com>
2023-09-05 04:48:09 -0700

    deleted route added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.59h68ks.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,9 +24,28 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const popularItemsCollection = client.db('banglaRestaurant').collection('popularItems');
+    const cartCollection = client.db('banglaRestaurant').collection('carts')
     app.get('/popularItems', async(req,res)=>{
         const result= await popularItemsCollection.find().toArray();
         res.send(result);
+    })
+    app.post('/carts', async(req,res)=>{
+        const item= req.body;
+        const result = await cartCollection.insertOne(item);
+        res.send(result);
+    
+    })
+    app.get('/carts', async(req,res)=>{
+      const result = await cartCollection.find().toArray();
+      res.send(result)
+    })
+    app.delete('/carts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+
+
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -42,6 +61,8 @@ run().catch(console.dir);
 app.get('/', (req,res)=>{
     res.send('Bangla Restaurant is running');
 })
+
+
 
 app.listen(port, ()=>{
     console.log(`Bangla restaurant is running on port ${port}`)
